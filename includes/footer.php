b8e31c8d64271a3637ebae307b6989c6b580d963
Razaan Kurniawan Putra <razaan.kp2005@gmail.com>
2023-10-17 10:02:11 +0700

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,11 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
 <<<<<<< HEAD
+<<<<<<< HEAD
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+=======
+>>>>>>> parent of 9f5082b (Menambahkan Inheritance Footer)
 =======
 >>>>>>> parent of 9f5082b (Menambahkan Inheritance Footer)
 </body>
