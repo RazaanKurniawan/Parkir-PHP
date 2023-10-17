dd86e6d27b8a15ec4885c39e733fd8dc0f258d71
Razaan Kurniawan Putra <razaan.kp2005@gmail.com>
2023-10-17 17:15:48 +0700

    Back Commit 2

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,5 @@
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-=======
->>>>>>> parent of 9f5082b (Menambahkan Inheritance Footer)
-=======
->>>>>>> parent of 9f5082b (Menambahkan Inheritance Footer)
 </body>
 </html>
